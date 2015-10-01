c65012fdc2a9d882d3801e5f6c1d3ed9b2491253
György Jánosi <gyuri0@prefixbox.com>
2015-10-01 14:46:06 +0200

    Add wizzair demo

---
+++ demo/wizzair/Wizz Air_files/home.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="http://ajax.microsoft.com/ajax/jquery/jquery-1.7.1.js" />
+
+(function ($)
+{
+    if (!$.browser.webkit && !$.browser.msie && !$.browser.mozilla && !$.browser.opera
+        || $.browser.msie && parseInt($.browser.version, 10) < 7
+        || $.browser.mozilla && parseInt($.browser.version, 10) < 9
+        || $.browser.opera && parseInt($.browser.version, 10) < 11)
+    {
+
+        $('#browserVersionError').show();
+
+        if ($.browser.msie)
+        {
+            $('#browserVersionIE').show();
+        }
+        if ($.browser.webkit)
+        {
+            $('#browserVersionChrome').show();
+        }
+        if ($.browser.mozilla)
+        {
+            $('#browserVersionMozilla').show();
+        }
+        if ($.browser.opera)
+        {
+            $('#browserVersionOpera').show();
+        }
+    }
+})(jQuery)
---
+++ demo/wizzair/Wizz Air_files/recaptcha.js
@@ -0,0 +1,28 @@
+﻿(function ()
+{
+    window.captchaIds = [];
+    window.initReCAPTCHA = function ()
+    {
+        var captchas = document.querySelectorAll('.g-recaptcha');
+        for (var i = 0; i < captchas.length; i++)
+        {
+            var captcha = captchas[i];
+            var widgetId = grecaptcha.render(captcha.id, { 'sitekey': captcha.getAttribute('data-sitekey') });
+            captcha.setAttribute('data-widget-id', widgetId);
+        }
+
+        window.setValidationGroupForReCAPTCHA = function ()
+        {
+            var captchas = document.querySelectorAll('.g-recaptcha');
+            for (var i = 0; i < captchas.length; i++)
+            {
+                var captcha = captchas[i];
+                var response = captcha.querySelector('.g-recaptcha-response');
+                if (response !== null && captcha.getAttribute('data-validation-group') !== null)
+                {
+                    response.setAttribute('data-validation-group', captcha.getAttribute('data-validation-group'));
+                }
+            }
+        }
+    }
+})();
